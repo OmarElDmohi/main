1fc74f231388dcd50aa1132c100e579225f1746e
ElDmohi <omareldmohi@gmail.com>
2018-03-20 23:35:11 +0200

    Practice File System + Rename|Copy|get|put

--- main.php
+++ main.php
@@ -1,21 +1,37 @@
 <?php
 
-    if(is_dir("Omar")){
+    if(file_exists(__DIR__ . "/practice")){
 
-        $omarFolderPath = dirname(__FILE__);
+       echo "I found it SIR...";
 
+    } else {
 
-        if(file_exists("$omarFolderPath/Omar/omarPass.txt")){
+        echo "Sorry,I didn't found it SIR...";
+        echo "<br>";
+        echo "I will make it...";
 
-            echo "yes I found this file";
-            file_put_contents("$omarFolderPath/Omar/omarPass.txt","hello from other world!");
-
-        } else{
-
-            echo "no I didn't found it, sorry omar..." . "<br>";
-            echo "BUT I WILL CREATE ONE FOR YOU <3";
-            file_put_contents("$omarFolderPath/Omar/omarPass.txt","hello from other world!");
-        }
-
+        mkdir(__DIR__ . '/practice');
     }
+
+    $myFolder = __DIR__ . "/practice";
+
+    file_put_contents($myFolder . "/myFile.php","I created my self !!!");
+
+    $myFile = $myFolder . "/myFile.php";
+
+    chmod($myFile, 0444);
+
+
+    if(is_writable($myFile)){
+
+        file_put_contents($myFile,"here is new lines");
+        echo "yes";
+
+    } else {
+
+        chmod($myFile,0777);
+        file_put_contents($myFile,"<?php echo 'hello from other world;'  ?>");
+    }
+
+    require_once($myFile);
 ?>
